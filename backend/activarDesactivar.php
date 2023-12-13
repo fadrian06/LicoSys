02d443d292370aba615512e418ef053d091bacb8
fadrian06 <franyeradriansanchez@gmail.com>
2023-12-13 02:36:31 -0400

    Added Sparrow dependency

--- backend/activarDesactivar.php
+++ backend/activarDesactivar.php
@@ -1,45 +1,102 @@
 <?php
 
-if (!empty($_POST)) :
-  require 'conexion.php';
-  require 'funciones.php';
+if (!$_POST) exit;
 
-  /** @var string La tabla a la que pertenece el registro. */
-  $tabla = escapar($_POST['tabla']);
-  /** @var string Campo que identifica cada registro. */
-  $campo = escapar($_POST['campo']);
-  /** @var string Valor único de cada registro. */
-  $valor = (int) escapar($_POST['valor']);
-  /** @var string 'activar' | 'desactivar' */
-  $accion = escapar($_POST['accion']);
+require_once __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/conexion.php';
+require __DIR__ . '/funciones.php';
 
-  switch ($tabla):
-    case 'usuarios':
-      $respuesta['ok'] = 'Usuario ';
-      break;
-    case 'negocios':
-      $respuesta['ok'] = 'Negocio ';
-      break;
-  endswitch;
+enum Action: string {
+  case Activate = 'activar';
+  case Disactivate = 'desactivar';
 
-  switch ($accion):
-    case 'activar':
-      $sql = "UPDATE $tabla SET activo=1 WHERE $campo=$valor";
-      $respuesta['ok'] .= 'activado exitósamente.';
-      break;
-    case 'desactivar':
-      $sql = "UPDATE $tabla SET activo=0 WHERE $campo=$valor";
-      $respuesta['ok'] .= 'desactivado exitósamente.';
-      break;
-    default:
-      $respuesta['error'] = "Por favor envie una opción ('activar' o 'desactivar')";
-  endswitch;
+  function toInt(): int {
+    return (int) ($this === self::Activate);
+  }
+}
 
-  if ($respuesta['error'])
-    exit(json_encode($respuesta, JSON_INVALID_UTF8_IGNORE));
+enum Table: string {
+  case Users = 'usuarios';
+  case Businesses = 'negocios';
+}
 
-  $resultado = setRegistro($sql);
-  if (!$resultado) $respuesta['error'] = $conexion->error;
+class JSONResponse {
+  private string $ok = '';
+  private string $error = '';
+  private array $data = [];
+  private int $code = 200;
 
-  exit(json_encode($respuesta, JSON_INVALID_UTF8_IGNORE));
-endif;
+  function setOkMessage(string $message): static {
+    $this->ok = $message;
+
+    return $this;
+  }
+
+  function getOkMessage(): string {
+    return $this->ok;
+  }
+
+  function setErrorMessage(string $error, int $code): static {
+    $this->error = $error;
+    $this->code = $code;
+
+    return $this;
+  }
+
+  function send(): never {
+    Flight::response()
+      ->status($this->code)
+      ->write(json_encode([
+        'ok' => $this->ok,
+        'error' => $this->error,
+        'datos' => $this->data
+      ]))
+      ->send();
+
+    exit;
+  }
+}
+
+$data = Flight::request()->data;
+$response = new JSONResponse;
+$db = new Sparrow;
+$db->setDb($conexion);
+
+try {
+  /** @var Table La tabla a la que pertenece el registro. */
+  $table = Table::from($data['tabla']);
+} catch (ValueError) {
+  $response
+    ->setErrorMessage("Tabla no soportada ('usuarios' | 'negocios')", 400)
+    ->send();
+}
+
+/** @var string Campo que identifica cada registro. */
+$field = escapar($data['campo']);
+/** @var int Valor único de cada registro. */
+$value = (int) escapar($data['valor']);
+
+try {
+  $action = Action::from($data['accion']);
+} catch (ValueError) {
+  $response
+    ->setErrorMessage("Por favor envie una opción válida ('activar' o 'desactivar')", 400)
+    ->send();
+}
+
+try {
+  $db->from($table->value)
+    ->where($field, $value)
+    ->update(['activo' => $action->toInt()])
+    ->execute();
+
+  $response->setOkMessage(sprintf(
+    '%s %s exitósamente.',
+    $table === Table::Users ? 'Usuario' : 'Negocio',
+    $action === Action::Activate ? 'activado' : 'desactivado'
+  ));
+} catch (Exception) {
+  $response->setErrorMessage('Ha ocurrido un error', 500);
+}
+
+$response->send();
