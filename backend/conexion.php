d496cd7f770de10e82264d34e64e1d1f66cc16c5
fadrian06 <franyeradriansanchez@gmail.com>
2024-06-11 00:03:12 -0400

    chore: replace db connection credentials consts to env variables

--- backend/conexion.php
+++ backend/conexion.php
@@ -1,5 +1,11 @@
 <?php
 
+use Symfony\Component\Dotenv\Dotenv;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+(new Dotenv)->load(__DIR__ . '/../.env');
+
 /**
  * Respuesta del servidor al cliente.
  * @var array{ok: string, error: string, datos: array}
@@ -10,32 +16,19 @@ $respuesta = [
 	'datos' => []
 ];
 
-// LOCAL
-const HOST    = 'localhost';
-const USUARIO = 'root';
-const CLAVE   = '';
-const BD      = 'licosys';
-const CHARSET = 'utf8';
-
-// ONLINE - FWHA
-// const USUARIO = '477828';
-// const CLAVE = 'fsanchez61001';
-// const BD = '477828';
-
-// ONLINE - 000webhost.com
-// const USUARIO = 'id20496120_fsanchez';
-// const CLAVE = 'tGB73Jd}mgcO$4I=';
-// const BD = 'id20496120_licosys';
-
-$conexion = @new MySQLi(HOST, USUARIO, CLAVE);
+$conexion = @new mysqli(
+	$_ENV['DB_HOST'],
+	$_ENV['DB_USERNAME'],
+	$_ENV['DB_PASSWORD']
+);
 
 if ($conexion->connect_errno)
 	exit("Error, no se pudo conectar a MySQL: <b>$conexion->error</b>");
 
-$conexion->set_charset(CHARSET);
+$conexion->set_charset('utf8');
 
 /*----------  Si no existe la base de datos, comienza la instalación  ----------*/
-if (!$conexion->select_db(BD))
+if (!$conexion->select_db($_ENV['DB_DATABASE']))
 	$mostrarLoader = '<script src="js/loader.js"></script>';
 
 /*----------  Instala la Base de Datos  ----------*/
