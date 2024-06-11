d330ed74ffd484aae8cd0618f18bd2bd4df66d5b
fadrian06 <franyeradriansanchez@gmail.com>
2024-06-10 23:41:07 -0400

    format: backend/conexion.php

--- backend/conexion.php
+++ backend/conexion.php
@@ -1,41 +1,42 @@
 <?php
-	/** @var array Respuesta del servidor al cliente. */
-	$respuesta = [
-		'ok'    => '',
-		'error' => '',
-		'datos' => []
-	];
 
-	// LOCAL
-	const HOST    = 'localhost';
-	const USUARIO = 'root';
-	const CLAVE   = '';
-	const BD      = 'licosys';
-	const CHARSET = 'utf8';
-	// ONLINE - FWHA
-	// const USUARIO = '477828';
-	// const CLAVE = 'fsanchez61001';
-	// const BD = '477828';
-	
-	// ONLINE - 000webhost.com
-	// const USUARIO = 'id20496120_fsanchez';
-	// const CLAVE = 'tGB73Jd}mgcO$4I=';
-	// const BD = 'id20496120_licosys';
+/** @var array Respuesta del servidor al cliente. */
+$respuesta = [
+	'ok'    => '',
+	'error' => '',
+	'datos' => []
+];
 
-	$conexion = @new MySQLi(HOST, USUARIO, CLAVE);
+// LOCAL
+const HOST    = 'localhost';
+const USUARIO = 'root';
+const CLAVE   = '';
+const BD      = 'licosys';
+const CHARSET = 'utf8';
 
-	if ($conexion->connect_errno)
-		exit("Error, no se pudo conectar a MySQL: <b>$conexion->error</b><br>");
+// ONLINE - FWHA
+// const USUARIO = '477828';
+// const CLAVE = 'fsanchez61001';
+// const BD = '477828';
 
-	$conexion->set_charset(CHARSET);
-	
-	/*----------  Si no existe la base de datos, comienza la instalación  ----------*/
-	if (!$conexion->select_db(BD))
-		$mostrarLoader = '<script src="js/loader.js"></script>';
-	
-	/*----------  Instala la Base de Datos  ----------*/
-	if (!empty($_POST['instalarBD'])):
-		$sql = file_get_contents('init.sql');
-		exit($conexion->multi_query($sql) ? 'true' : $conexion->error);
-	endif;
-?>
+// ONLINE - 000webhost.com
+// const USUARIO = 'id20496120_fsanchez';
+// const CLAVE = 'tGB73Jd}mgcO$4I=';
+// const BD = 'id20496120_licosys';
+
+$conexion = @new MySQLi(HOST, USUARIO, CLAVE);
+
+if ($conexion->connect_errno)
+	exit("Error, no se pudo conectar a MySQL: <b>$conexion->error</b><br>");
+
+$conexion->set_charset(CHARSET);
+
+/*----------  Si no existe la base de datos, comienza la instalación  ----------*/
+if (!$conexion->select_db(BD))
+	$mostrarLoader = '<script src="js/loader.js"></script>';
+
+/*----------  Instala la Base de Datos  ----------*/
+if (!empty($_POST['instalarBD'])) :
+	$sql = file_get_contents('init.sql');
+	exit($conexion->multi_query($sql) ? 'true' : $conexion->error);
+endif;
