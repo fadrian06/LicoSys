07862e37d09a66de787412160604cde1354e8697
fadrian06 <franyeradriansanchez@gmail.com>
2025-08-13 01:26:18 -0400

    reuse conexion.php in /api/instalar-bd

--- api/index.php
+++ api/index.php
@@ -5,16 +5,11 @@ declare(strict_types=1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 Flight::route('POST /instalar-bd', function (): void {
-  $mysqli = new mysqli(
-    $_ENV['DB_HOST'],
-    $_ENV['DB_USERNAME'],
-    $_ENV['DB_PASSWORD'],
-  );
+  require_once BASE_DIR . '/backend/conexion.php';
 
-  $mysqli->set_charset('utf8');
   $sql = file_get_contents(BASE_DIR . '/database/init.mysql.sql');
 
-  exit($mysqli->multi_query($sql ?: '') ? 'true' : $mysqli->error);
+  exit($conexion->multi_query($sql ?: '') ? 'true' : $conexion->error);
 });
 
 Flight::start();
