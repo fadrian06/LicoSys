846e50ff313bc6778b0ce615335990ba6ac371eb
fadrian06 <franyeradriansanchez@gmail.com>
2025-08-12 23:51:51 -0400

    fix undefined function respaldarBD and restaurarBD

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,4 +1,6 @@
-import { modal, vaciarLog } from "./funciones";
+import { modal, respaldarBD, restaurarBD, vaciarLog } from "./funciones";
 
 globalThis.modal = modal;
 globalThis.vaciarLog = vaciarLog;
+globalThis.respaldarBD = respaldarBD;
+globalThis.restaurarBD = restaurarBD;
