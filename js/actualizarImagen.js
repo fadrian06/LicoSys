7d73056d1a327f9d1dfb33eb97bcd20d0da4aa61
fadrian06 <franyeradriansanchez@gmail.com>
2026-06-04 21:37:10 -0400

    Recompilados los assets

--- js/actualizarImagen.js
+++ js/actualizarImagen.js
@@ -1,6 +1,4 @@
-"use strict";
-
-/**
+"use strict";/**
  * Esta funciona vigila un cambio de imagen y valida si cumple algunos requisitos:
  * <ul>
  * <li>La imagen sólo puede tener formato JPG o PNG</li>
@@ -9,19 +7,4 @@
  * @param  {HTMLInputElement} input `<input type="file">`
  * @param  {HTMLImageElement} image La `<img>` a actualizar
  * @param {(error: string)} [cb] Una función que se ejecuta si algo sale mal. <i>(opcional)</i>
- */
-var actualizarImagen = function actualizarImagen(input, image) {
-  var cb = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : function () {};
-  input.onchange = function () {
-    var file = input.files[0];
-    if (file.type !== 'image/jpeg' && file.type !== 'image/jpg' && file.type !== 'image/png') return cb('Sólo se permiten imagenes JPG y PNG');
-    if (file.size > 1 * 1000 * 1024 * 2) /*1b * 1000 = 1kb * 1024 = 1mb * 2 = :D*/
-      return cb('La imagen no puede ser mayor a 2MB');
-    var fileReader = new FileReader();
-    fileReader.readAsDataURL(file);
-    fileReader.onload = function (e) {
-      image.setAttribute('src', e.target.result);
-      return cb();
-    };
-  };
-};
+ */var actualizarImagen=function actualizarImagen(input,image){var cb=arguments.length>2&&arguments[2]!==undefined?arguments[2]:function(){};input.onchange=function(){var file=input.files[0];if(file.type!=="image/jpeg"&&file.type!=="image/jpg"&&file.type!=="image/png")return cb("S\xF3lo se permiten imagenes JPG y PNG");if(file.size>1*1000*1024*2)/*1b * 1000 = 1kb * 1024 = 1mb * 2 = :D*/return cb("La imagen no puede ser mayor a 2MB");var fileReader=new FileReader;fileReader.readAsDataURL(file);fileReader.onload=function(e){image.setAttribute("src",e.target.result);return cb()}}};
--- js/login.js
+++ js/login.js
@@ -1,91 +1,10 @@
-"use strict";
-
-/** @typedef {import('./funciones')} */
-
-/*=====================================
+"use strict";/** @typedef {import('./funciones')} */ /*=====================================
 =            DECLARACIONES            =
-=====================================*/
-/** @type {HTMLDivElement} */
-var contenedorReloj = document.querySelector('.reloj');
-/** @type {HTMLFormElement} */
-var form = document.querySelector('#login');
-/** @type {HTMLElement} */
-var usuarioLoader = form.querySelector('#usuarioLoader');
-/*=====  End of DECLARACIONES  ======*/
-
-/*==============================================
+=====================================*/ /** @type {HTMLDivElement} */var contenedorReloj=document.querySelector(".reloj");/** @type {HTMLFormElement} */var form=document.querySelector("#login");/** @type {HTMLElement} */var usuarioLoader=form.querySelector("#usuarioLoader");/*=====  End of DECLARACIONES  ======*/ /*==============================================
 =            EJECUCIÓN DE FUNCIONES            =
-==============================================*/
-reloj(contenedorReloj);
-setInterval(function () {
-  return reloj(contenedorReloj);
-}, 1 * 1000 * 60 /*1 minuto*/);
-verClave(form.clave.nextElementSibling, form.clave);
-new Typed('#typed', {
-  strings: ['<i>Sencillo.</i>', '<i>Rápido.</i>', '<i>Moderno.</i>', '<i>Seguro.</i>', '<i>Completo.</i>'],
-  typeSpeed: 100,
-  startDelay: 1000,
-  backSpeed: 50,
-  loop: true,
-  cursorChar: '<i class="w3-medium icon-chevron-left"></i>'
-});
-form.usuario.addEventListener('blur', function () {
-  if (!form.usuario.value) return;
-  usuarioLoader.classList.remove('w3-hide');
-  usuarioLoader.classList.add('w3-show');
-  var post = {
-    verificarUsuario: true,
-    usuario: form.usuario.value
-  };
-  $.post('backend/login.php', post, function (res) {
-    /** @type {Respuesta} */
-    var datos = JSON.parse(res);
-    if (datos.error) return alerta(datos.error).on('onShow', function () {
-      usuarioLoader.classList.remove('w3-show');
-      usuarioLoader.classList.add('w3-hide');
-      form.usuario.parentElement.parentElement.classList.remove('valido');
-      form.usuario.parentElement.parentElement.classList.add('invalido');
-    }).show();
-    var spinner = usuarioLoader.querySelector('i');
-    spinner.classList.remove('icon-spinner', 'w3-spin');
-    spinner.classList.add('icon-check', 'w3-text-green');
-    form.usuario.parentElement.parentElement.classList.add('valido');
-  });
-});
-var intentos = 0;
-validar(form, function (error, fd, e) {
-  if (error) return alerta(error).show();
-  e.preventDefault();
-  form.classList.add('showLoader');
-  fd.append('login', true);
-  ajax('backend/login.php', fd, function (res) {
-    /** @type {Respuesta} */
-    var datos = JSON.parse(res);
-    if (datos.error) {
-      var text = datos.error;
-      if (datos.error === 'Contraseña incorrecta') ++intentos;
-      if (intentos <= 3 && intentos > 0) text += " <strong>(intento: ".concat(intentos, " / 3)</strong>");else {
-        /**
-        
-        	TODO:
-        	- Bloquear a los 3 intentos
-        
-         */
-        intentos = 0;
-      }
-      return alerta(text).on('onShow', function () {
-        return form.classList.remove('showLoader');
-      }).show();
-    }
-    form.classList.remove('showLoader');
-    var href = location.href;
-    form.parentElement.classList.add('showLoader');
-    if (!href.indexOf('index.php')) return location.href += 'dashboard.php';
-    href = href.replace(/index\.php/g, function (coincidencia) {
-      coincidencia = 'dashboard.php';
-      return coincidencia;
-    });
-    return location.href = href;
-  });
-});
-/*=====  End of EJECUCIÓN DE FUNCIONES  ======*/
+==============================================*/reloj(contenedorReloj);setInterval(function(){return reloj(contenedorReloj)},1*1000*60/*1 minuto*/);verClave(form.clave.nextElementSibling,form.clave);new Typed("#typed",{strings:["<i>Sencillo.</i>","<i>R\xE1pido.</i>","<i>Moderno.</i>","<i>Seguro.</i>","<i>Completo.</i>"],typeSpeed:100,startDelay:1000,backSpeed:50,loop:true,cursorChar:"<i class=\"w3-medium icon-chevron-left\"></i>"});form.usuario.addEventListener("blur",function(){if(!form.usuario.value)return;usuarioLoader.classList.remove("w3-hide");usuarioLoader.classList.add("w3-show");var post={verificarUsuario:true,usuario:form.usuario.value};$.post("backend/login.php",post,function(res){/** @type {Respuesta} */var datos=JSON.parse(res);if(datos.error)return alerta(datos.error).on("onShow",function(){usuarioLoader.classList.remove("w3-show");usuarioLoader.classList.add("w3-hide");form.usuario.parentElement.parentElement.classList.remove("valido");form.usuario.parentElement.parentElement.classList.add("invalido")}).show();var spinner=usuarioLoader.querySelector("i");spinner.classList.remove("icon-spinner","w3-spin");spinner.classList.add("icon-check","w3-text-green");form.usuario.parentElement.parentElement.classList.add("valido")})});var intentos=0;validar(form,function(error,fd,e){if(error)return alerta(error).show();e.preventDefault();form.classList.add("showLoader");fd.append("login",true);ajax("backend/login.php",fd,function(res){/** @type {Respuesta} */var datos=JSON.parse(res);if(datos.error){var text=datos.error;if(datos.error==="Contrase\xF1a incorrecta")++intentos;if(intentos<=3&&intentos>0)text+=" <strong>(intento: ".concat(intentos," / 3)</strong>");else{/**
+			
+					TODO:
+					- Bloquear a los 3 intentos
+			
+				 */intentos=0}return alerta(text).on("onShow",function(){return form.classList.remove("showLoader")}).show()}form.classList.remove("showLoader");var href=location.href;form.parentElement.classList.add("showLoader");if(!href.indexOf("index.php"))return location.href+="dashboard.php";href=href.replace(/index\.php/g,function(coincidencia){coincidencia="dashboard.php";return coincidencia});return location.href=href})});/*=====  End of EJECUCIÓN DE FUNCIONES  ======*/
