cf7754167c150341614c2d89df1cc5461fe13635
fadrian06 <franyeradriansanchez@gmail.com>
2026-06-05 01:39:13 -0400

    Mejorado el formato de index.php

--- index.php
+++ index.php
@@ -1,67 +1,66 @@
 <?php
-	/*======================================
-	=            LÓGICA INICIAL            =
-	======================================*/
-	session_start();
-	if (isset($_SESSION['activa'])) header('location: dashboard.php');
 
-	include 'templates/head.php';
+require_once 'vendor/autoload.php';
 
-	if (!empty($_SESSION['userID']))$_SESSION['userID'] = $admin['id'];
+/*======================================
+=            LÓGICA INICIAL            =
+======================================*/
+session_start();
 
-	setRegistro('TRUNCATE TABLE carrito_venta');
-	setRegistro('TRUNCATE TABLE carrito_compra');
+if (isset($_SESSION['activa'])) header('location: dashboard.php');
 
-	function verificarCopiaDeSeguridad() {
-		global $script;
-		if (file_exists('backup/backup.sql'))
-			$script .= '<script src="js/restaurarBD.js"></script>';
-	}
-	/*=====  End of LÓGICA INICIAL  ======*/
+include 'templates/head.php';
 
-	/*----------  Si no hay negocios, solicita registro  ----------*/
-	if (!isset($mostrarLoader) and !$negocios):
+if (!empty($_SESSION['userID'])) $_SESSION['userID'] = $admin['id'];
 
-		verificarCopiaDeSeguridad();
-		$mostrarRegistro = true;
-		include 'templates/registrarNegocio.php';
-		$script .= '<script src="js/registrarNegocio.js"></script>';
+setRegistro('TRUNCATE TABLE carrito_venta');
+setRegistro('TRUNCATE TABLE carrito_compra');
 
-	/*----------  Si no hay administrador, solicita registro  ----------*/
-	elseif (!isset($mostrarLoader) and !$admin):
+function verificarCopiaDeSeguridad() {
+	global $script;
 
-		verificarCopiaDeSeguridad();
-		$mostrarRegistro = true;
-		include 'templates/registrarAdmin.php';
-		$script .= '<script src="js/registrarAdmin.js"></script>';
+	if (file_exists('backup/backup.sql'))
+		$script .= '<script src="js/restaurarBD.js"></script>';
+}
+/*=====  End of LÓGICA INICIAL  ======*/
 
-	/*----------  Si el administrador no tiene preguntas secretas, solicita registro  ----------*/
-	elseif (!isset($mostrarLoader) and !$admin['pre1']):
+/*----------  Si no hay negocios, solicita registro  ----------*/
+if (!isset($mostrarLoader) and !$negocios):
+	verificarCopiaDeSeguridad();
+	$mostrarRegistro = true;
+	include 'templates/registrarNegocio.php';
+	$script .= '<script src="js/registrarNegocio.js"></script>';
 
-		verificarCopiaDeSeguridad();
-		$mostrarRegistro = true;
-		include 'templates/registroPreguntasRespuestas.php';
-		$script .= '<script src="js/registrarPreguntasRespuestas.js"></script>';
+/*----------  Si no hay administrador, solicita registro  ----------*/
+elseif (!isset($mostrarLoader) and !$admin):
+	verificarCopiaDeSeguridad();
+	$mostrarRegistro = true;
+	include 'templates/registrarAdmin.php';
+	$script .= '<script src="js/registrarAdmin.js"></script>';
 
-	/*----------  Muestra el login  ----------*/
-	elseif (!isset($mostrarLoader)):
+/*----------  Si el administrador no tiene preguntas secretas, solicita registro  ----------*/
+elseif (!isset($mostrarLoader) and !$admin['pre1']):
+	verificarCopiaDeSeguridad();
+	$mostrarRegistro = true;
+	include 'templates/registroPreguntasRespuestas.php';
+	$script .= '<script src="js/registrarPreguntasRespuestas.js"></script>';
 
-		$mostrarLogin = true;
-		include 'templates/login.php';
-		include 'templates/consultarPreguntasRespuestas.php';
+/*----------  Muestra el login  ----------*/
+elseif (!isset($mostrarLoader)):
+	$mostrarLogin = true;
+	include 'templates/login.php';
+	include 'templates/consultarPreguntasRespuestas.php';
 
-		if (isset($_SESSION['showQuestions']))
-			include 'templates/preguntasRespuestas.php';
+	if (isset($_SESSION['showQuestions']))
+		include 'templates/preguntasRespuestas.php';
 
-		if (isset($_SESSION['changePassword']))
-			include 'templates/cambiarClave.php';
+	if (isset($_SESSION['changePassword']))
+		include 'templates/cambiarClave.php';
 
-		$script .= '<script src="js/reloj.js"></script>';
-		$script .= '<script src="libs/typedjs/typed.min.js"></script>';
-		$script .= '<script src="js/login.js"></script>';
-		$script .= '<script src="js/recuperarClave.js"></script>';
+	$script .= '<script src="js/reloj.js"></script>';
+	$script .= '<script src="libs/typedjs/typed.min.js"></script>';
+	$script .= '<script src="js/login.js"></script>';
+	$script .= '<script src="js/recuperarClave.js"></script>';
+endif;
 
-	endif;
-
-	include 'templates/footer.php';
-?>
+include 'templates/footer.php';
