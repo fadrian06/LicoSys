8430bc1d4288f1a78fe680a44a324c3525a23f05
fadrian06 <franyeradriansanchez@gmail.com>
2026-06-22 22:51:25 -0400

    Simplificada la función getenv

--- app/functions.php
+++ app/functions.php
@@ -5,23 +5,13 @@ declare(strict_types=1);
 namespace App;
 
 use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\Dotenv\Exception\FormatException;
-use Symfony\Component\Dotenv\Exception\PathException;
 
-/** @throws FormatException | PathException */
-function getenv(string $name): string|int|null
+function getenv(string $name): string
 {
   if (!array_key_exists($name, $_ENV)) {
     $dotenv = new Dotenv;
-    $dotenv->load(__DIR__ . '/../.env.example');
-    $dotenv->overload(__DIR__ . '/../.env');
+    $dotenv->load(__DIR__ . '/../.env.example', __DIR__ . '/../.env');
   }
 
-  $env = $_ENV[$name] ?? null;
-
-  if (filter_var($env, FILTER_VALIDATE_INT)) {
-    $env = intval(filter_var($env, FILTER_VALIDATE_INT));
-  }
-
-  return is_string($env) || is_int($env) ? $env : null;
+  return $_ENV[$name] ?? null;
 }
