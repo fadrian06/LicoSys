5b22dd202aaa0a127e21ef5668b8b43340f3b545
fadrian06 <franyeradriansanchez@gmail.com>
2026-06-25 19:42:23 -0400

    composer require -W --fixed psr/http-server-handler psr/http-server-middleware

---
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Override;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final readonly class RedirectIfAuthenticated implements MiddlewareInterface
+{
+  public function __construct(private ResponseFactoryInterface $responseFactory) {}
+
+  #[Override]
+  public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+  {
+    if (session_status() === PHP_SESSION_NONE) {
+      session_start();
+    }
+
+    if (!empty($_SESSION['activa'])) {
+      return $this->responseFactory->createResponse()->withHeader('location', 'dashboard.php');
+    }
+
+    return $handler->handle($request);
+  }
+}
--- index.php
+++ index.php
@@ -2,27 +2,35 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\IndexController;
+use App\Http\Middleware\RedirectIfAuthenticated;
 use GuzzleHttp\Psr7\HttpFactory;
+use GuzzleHttp\Psr7\ServerRequest;
 use Illuminate\Container\Container;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-Container::getInstance()->singleton(ResponseFactoryInterface::class, HttpFactory::class);
+Container::getInstance()->singletonIf(ResponseFactoryInterface::class, HttpFactory::class);
+Container::getInstance()->singletonIf(ServerRequestInterface::class, ServerRequest::fromGlobals(...));
+
+$redirectIfAuthenticated = Container::getInstance()->get(RedirectIfAuthenticated::class);
+$indexController = Container::getInstance()->get(IndexController::class);
+$response = Container::getInstance()->call($redirectIfAuthenticated->process(...), ['handler' => $indexController]);
+
+if ($response instanceof ResponseInterface) {
+  foreach ($response->getHeaders() as $name => $values) {
+    header("$name: " . join(', ', $values));
+  }
+
+  echo $response->getBody();
+}
 
 /*======================================
 =            LÓGICA INICIAL            =
 ======================================*/
-/** @var ?ResponseInterface */
-$response = require __DIR__ . '/app/Http/Middlewares/RedirectIfAuthenticated.php';
-
-if ($response instanceof ResponseInterface) {
-	foreach ($response->getHeaders() as $name => $value) {
-		header($name . ': ' . implode(', ', $value));
-	}
-}
-
 include 'templates/head.php';
 
 if (!empty($_SESSION['userID'])) $_SESSION['userID'] = $admin['id'];
@@ -32,50 +40,49 @@ setRegistro('TRUNCATE TABLE carrito_compra');
 
 function verificarCopiaDeSeguridad()
 {
-	global $script;
+ global $script;
 
-	if (file_exists('backup/backup.sql'))
-		$script .= '<script src="resources/build/restaurarBD.js"></script>';
+ if (file_exists('backup/backup.sql'))
+   $script .= '<script src="resources/build/restaurarBD.js"></script>';
 }
-/*=====  End of LÓGICA INICIAL  ======*/
 
 /*----------  Si no hay negocios, solicita registro  ----------*/
 if (!isset($mostrarLoader) and !$negocios):
-	verificarCopiaDeSeguridad();
-	$mostrarRegistro = true;
-	include 'templates/registrarNegocio.php';
-	$script .= '<script src="resources/build/registrarNegocio.js"></script>';
+ verificarCopiaDeSeguridad();
+ $mostrarRegistro = true;
+ include 'templates/registrarNegocio.php';
+ $script .= '<script src="resources/build/registrarNegocio.js"></script>';
 
 /*----------  Si no hay administrador, solicita registro  ----------*/
 elseif (!isset($mostrarLoader) and !$admin):
-	verificarCopiaDeSeguridad();
-	$mostrarRegistro = true;
-	include 'templates/registrarAdmin.php';
-	$script .= '<script src="resources/build/registrarAdmin.js"></script>';
+ verificarCopiaDeSeguridad();
+ $mostrarRegistro = true;
+ include 'templates/registrarAdmin.php';
+ $script .= '<script src="resources/build/registrarAdmin.js"></script>';
 
 /*----------  Si el administrador no tiene preguntas secretas, solicita registro  ----------*/
 elseif (!isset($mostrarLoader) and !$admin['pre1']):
-	verificarCopiaDeSeguridad();
-	$mostrarRegistro = true;
-	include 'templates/registroPreguntasRespuestas.php';
-	$script .= '<script src="resources/build/registrarPreguntasRespuestas.js"></script>';
+ verificarCopiaDeSeguridad();
+ $mostrarRegistro = true;
+ include 'templates/registroPreguntasRespuestas.php';
+ $script .= '<script src="resources/build/registrarPreguntasRespuestas.js"></script>';
 
 /*----------  Muestra el login  ----------*/
 elseif (!isset($mostrarLoader)):
-	$mostrarLogin = true;
-	include 'templates/login.php';
-	include 'templates/consultarPreguntasRespuestas.php';
+ $mostrarLogin = true;
+ include 'templates/login.php';
+ include 'templates/consultarPreguntasRespuestas.php';
 
-	if (isset($_SESSION['showQuestions']))
-		include 'templates/preguntasRespuestas.php';
+ if (isset($_SESSION['showQuestions']))
+   include 'templates/preguntasRespuestas.php';
 
-	if (isset($_SESSION['changePassword']))
-		include 'templates/cambiarClave.php';
+ if (isset($_SESSION['changePassword']))
+   include 'templates/cambiarClave.php';
 
-	$script .= '<script src="resources/libs/typedjs/typed.min.js"></script>';
-	$script .= '<script src="resources/build/reloj.js"></script>';
-	$script .= '<script src="resources/build/login.js"></script>';
-	$script .= '<script src="resources/build/recuperarClave.js"></script>';
+ $script .= '<script src="resources/libs/typedjs/typed.min.js"></script>';
+ $script .= '<script src="resources/build/reloj.js"></script>';
+ $script .= '<script src="resources/build/login.js"></script>';
+ $script .= '<script src="resources/build/recuperarClave.js"></script>';
 endif;
 
 include 'templates/footer.php';
