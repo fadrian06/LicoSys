93392c5abeacd9b5c46fc068beec0d3ea58f6384
fadrian06 <franyeradriansanchez@gmail.com>
2026-07-12 01:49:13 -0400

    refactorizado la inyección de scripts en head.php para utilizar Scripts::pushInline y mejorar la legibilidad del código

--- templates/head.php
+++ templates/head.php
@@ -40,64 +40,74 @@ if ($path === '/dashboard.php' && key_exists('userID', $_SESSION)):
     strtolower($usuario['pre1']) === 'no especificada' || !$usuario['pre1']
     || strtolower($usuario['pre2']) === 'no especificada' || !$usuario['pre2']
     || strtolower($usuario['pre3']) === 'no especificada' || !$usuario['pre3']
-  ) Scripts::push(<<<HTML
-    <script>
-      let textoNoTienesPreguntasNiRespuestas = `
-        <strong class="w3-text-red">
-          No tienes preguntas y respuestas registradas.
-        </strong><br>
-        <small>¿Desea registrarlas?</small>
-      `
+  ) Scripts::pushInline(<<<JS
+    let textoNoTienesPreguntasNiRespuestas = `
+      <strong class="w3-text-red">
+        No tienes preguntas y respuestas registradas.
+      </strong><br>
+      <small>¿Desea registrarlas?</small>
+    `
 
-      confirmar(textoNoTienesPreguntasNiRespuestas, 'center', () => {
-        $('[href="views/miPerfil.php"]')[0].click()
-        let intervalo = setInterval(() => {
-          if ($('#moduloPerfil')[0]) {
-            $('[role="botonPanel"]:last-child')[0].click()
-            $('[data-target="#editarPreguntasRespuestas"]')[0].click()
-            clearInterval(intervalo)
-          }
-        }, 500)
-      })
-    </script>
-  HTML);
+    confirmar(textoNoTienesPreguntasNiRespuestas, 'center', () => {
+      $('[href="views/miPerfil.php"]')[0].click()
+
+      let intervalo = setInterval(() => {
+        if ($('#moduloPerfil')[0]) {
+          $('[role="botonPanel"]:last-child')[0].click()
+          $('[data-target="#editarPreguntasRespuestas"]')[0].click()
+
+          clearInterval(intervalo)
+        }
+      }, 500)
+    })
+  JS);
 
   /*----------  Inventario agotado  ----------*/
-  $productos = $manager::table('inventario')->get(['id', 'producto', 'stock'])->toArray();
+  $productos = $manager::table('inventario')
+    ->get(['id', 'producto', 'stock'])
+    ->toArray();
 
-  foreach ($productos as $i => $producto):
+  foreach ($productos as $index => $producto):
     $producto = (array) $producto;
     $tiempo = 1000 * 60; /*60 segundos*/
 
     if (!$producto['stock'])
-      Scripts::push(<<<HTML
-        <script>
-          setTimeout(() => alerta('{$producto['producto']} está AGOTADO').show(), 3000)
+      Scripts::pushInline(<<<JS
+        setTimeout(
+          () => alerta('{$producto['producto']} está AGOTADO').show(),
+          3000,
+        )
 
-          let intervalo{$i} = setInterval(() => {
-            alerta('{$producto['producto']} está AGOTADO').show()
-          }, $tiempo)
+        let intervalo{$index} = setInterval(() => {
+          alerta('{$producto['producto']} está AGOTADO').show()
+        }, $tiempo)
 
-          setTimeout(() => clearInterval(intervalo{$i}), $tiempo * 10 /* 10 minutos */)
-        </script>
-      HTML);
+        setTimeout(
+          () => clearInterval(intervalo{$index}),
+          $tiempo * 10 /* 10 minutos */
+        )
+      JS);
     elseif ($producto['stock'] <= 5)
-      Scripts::push(<<<HTML
-        <script>
-          setTimeout(() => advertencia('{$producto['producto']} CASI AGOTADO').show(), 3000)
+      Scripts::pushInline(<<<JS
+        setTimeout(
+          () => advertencia('{$producto['producto']} CASI AGOTADO').show(),
+          3000,
+        )
 
-          let intervalo{$i} = setInterval(() => {
-            advertencia('{$producto['producto']} CASI AGOTADO').show()
-          }, $tiempo)
+        let intervalo{$i} = setInterval(() => {
+          advertencia('{$producto['producto']} CASI AGOTADO').show()
+        }, $tiempo)
 
-          setTimeout(() => clearInterval(intervalo{$i}), $tiempo * 10 /* 5 minutos */)
-        </script>
-      HTML);
+        setTimeout(
+          () => clearInterval(intervalo{$i}),
+          $tiempo * 10, /* 5 minutos */
+        )
+      JS);
   endforeach;
 endif;
 
 /*====================================================================
-=            LÓGICA DE TODO EL SISTEMA, INCLUIDO EL LOGIN            =
+=            LÓGICA DE TOD0 EL SISTEMA, INCLUIDO EL LOGIN            =
 ====================================================================*/
 $negocios = [];
 $admin = [];
@@ -105,7 +115,11 @@ $productosEnCarrito = 0;
 $productosEnCarritoCompra = 0;
 
 try {
-  $negocios = $manager::table('negocios')->where('activo', true)->get()->toArray();
+  $negocios = $manager::table('negocios')
+    ->where('activo', true)
+    ->get()
+    ->toArray();
+
   $admin = (array) $manager::table('usuarios')->where('cargo', 'a')->first();
   $productosEnCarrito = $manager::table('carrito_venta')->count();
   $productosEnCarritoCompra = $manager::table('carrito_compra')->count();
@@ -113,23 +127,27 @@ try {
   get_exception_handler()($exception);
 }
 
+$baseHref = str_replace(['index.php', 'dashboard.php'], '', $_SERVER['SCRIPT_NAME']);
+
 ?>
 
-<!DOCTYPE html>
-<html lang="es" data-app-name="<?= getenv('APP_NAME') ?>">
+<!doctype html>
+<html
+  lang="<?= getenv('APP_LOCALE') ?>"
+  data-app-name="<?= getenv('APP_NAME') ?>">
 
   <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="theme-color" content="black">
-    <meta name="color-scheme" content="light dark">
-    <base href="<?= str_replace(['index.php', 'dashboard.php'], '', $_SERVER['SCRIPT_NAME']) ?>">
-    <link rel="icon" href="./resources/images/logo.png">
-    <link rel="stylesheet" href="./resources/icons/style.min.css">
-    <link rel="stylesheet" href="./resources/fonts/fuentes.css">
-    <link rel="stylesheet" href="./resources/libs/noty/noty.css">
-    <link rel="stylesheet" href="./resources/libs/noty/themes/sunset.css">
-    <link rel="stylesheet" href="./resources/build/index.css">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <meta name="theme-color" content="black" />
+    <meta name="color-scheme" content="light dark" />
+    <base href="<?= $baseHref ?>" />
+    <link rel="icon" href="./resources/images/logo.png" />
+    <link rel="stylesheet" href="./resources/icons/style.min.css" />
+    <link rel="stylesheet" href="./resources/fonts/fuentes.css" />
+    <link rel="stylesheet" href="./resources/libs/noty/noty.css" />
+    <link rel="stylesheet" href="./resources/libs/noty/themes/sunset.css" />
+    <link rel="stylesheet" href="./resources/build/index.css" />
     <title><?= getenv('APP_NAME')  ?></title>
     <script src="./resources/libs/jquery.min.js"></script>
     <script src="./resources/libs/w3/w3.min.js"></script>
