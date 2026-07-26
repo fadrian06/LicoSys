1630a5ec1e043f51d014cfbdf8322613eeea77f1
fadrian06 <franyeradriansanchez@gmail.com>
2026-07-25 20:12:35 -0400

    añadidos DecoratingRequestHandler y QueueRequestHandler

---
+++ app/DecoratingRequestHandler.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use NoDiscard;
+use Override;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final readonly class DecoratingRequestHandler implements RequestHandlerInterface
+{
+  public function __construct(
+    private MiddlewareInterface $middleware,
+    private RequestHandlerInterface $nextHandler,
+  ) {}
+
+  #[Override]
+  #[NoDiscard]
+  public function handle(ServerRequestInterface $request): ResponseInterface
+  {
+    return $this->middleware->process($request, $this->nextHandler);
+  }
+}
---
+++ app/QueueRequestHandler.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use NoDiscard;
+use Override;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class QueueRequestHandler implements RequestHandlerInterface
+{
+  /** @var MiddlewareInterface[] */
+  private array $middlewares = [];
+
+  public function __construct(private readonly RequestHandlerInterface $fallbackHandler) {}
+
+  public function add(MiddlewareInterface $middleware): self
+  {
+    $this->middlewares[] = $middleware;
+
+    return $this;
+  }
+
+  #[Override]
+  #[NoDiscard]
+  public function handle(ServerRequestInterface $request): ResponseInterface
+  {
+    $middleware = array_shift($this->middlewares);
+
+    return $middleware
+      ? $middleware->process($request, $this)
+      : $this->fallbackHandler->handle($request);
+  }
+}
